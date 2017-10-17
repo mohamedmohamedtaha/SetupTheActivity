51ed51bf326f356780d8abc8c21c840d0fab7438
MohamedTaha <mohamed01007919166@gmail.com>
2017-10-17 22:11:29 +0300

    the nine edit

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -8,13 +8,16 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
 
 /**
  * Created by MANASATT on 16/10/17.
  */
 // TODO (23) Implement OnSharedPreferenceChangeListener
+// TODO (30) Implement OnPreferenceChangeListener
 
-public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener,
+      Preference.OnPreferenceChangeListener{
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualizer);
@@ -31,7 +34,10 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
                 setPreferenceSummary(preference,value);
             }
 
-        }    }
+        }
+         Preference preference = findPreference(getString(R.string.size_key));
+        preference.setOnPreferenceChangeListener(this);
+    }
     // TODO (29) Don't forget to add code here to properly set the summary for an EditTextPreference
 
     private void setPreferenceSummary(Preference preference, String value){
@@ -80,6 +86,33 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     public void onDestroy() {
         super.onDestroy();
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+
+    // TODO (31) Override onPreferenceChange. This method should try to convert the new preference value
+// to a float; if it cannot, show a helpful error message and return false. If it can be converted
+    // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
+    // an error message and return false. If it is a valid number, return true.
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+       Toast error = Toast.makeText( getContext(), "Please select a number between 0.1 and 3", Toast.LENGTH_SHORT);
+
+        String sizeKey = getString(R.string.size_key);
+        if (preference.getKey().equals(sizeKey)){
+            String stringSize = ((String)(newValue)).trim();
+            if (stringSize.equals("")) stringSize = "1";
+            try{
+                float size = Float.parseFloat(stringSize);
+                if (size >3 || size <= 0){
+                    error.show();
+                    return false;
+                }
+            }catch (NumberFormatException e){
+                error.show();
+                return false;
+            }
+        }
+        return true;
     }
 }
 
